fc9142a221e59cd7468bc219651c52bd02c76595
Harsh Kashyap <harshkashyap307@gmail.com>
2020-12-11 10:29:52 +0530

    Add Login-Logout-GetInfo routes for User

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -30,7 +30,6 @@ const userCtrl = {
            res.cookie('refreshtoken', refreshtoken, {
                httpOnly: true,
                path: '/user/refresh_token',
-               maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
            })
 
            res.json({accesstoken});
@@ -41,6 +40,45 @@ const userCtrl = {
             return res.status(500).json({msg:err.message})
         }
     },
+
+    login: async (req, res) =>{
+        try{
+            const {email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(!user)return res.status(400).json({msg:"User doesn't exist"})
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch) return res.status(400).json({msg:"Incorrect password."})
+
+            // if login success, create access tokan and refresh token
+            const accesstoken = createAccessToken({id: user._id});
+            const refreshtoken = createRefreshToken({id: user._id});
+
+            res.cookie('refreshtoken', refreshtoken, {
+               httpOnly: true,
+               path: '/user/refresh_token',
+            })
+
+            res.json({accesstoken});
+
+        }catch(err){
+            return res.status(500).json({msg:err.msg})
+        }
+    },
+
+    logout: async (req, res) =>{
+        try{
+          res.clearCookie('refreshtoken', {path: '/user/refresh_token'});
+
+          return res.json({msg:"Logged out"});
+
+
+        } catch(err){
+            return res.status(500).json({msg:err.msg})
+        }
+    },
+
     refreshToken: (req, res) =>{
 
         try{
@@ -58,7 +96,19 @@ const userCtrl = {
         } catch(err){
             return res.status(500).json({msg:err.msg})
         }
+    },
+
+    getUser : async(req, res) =>{
+        try{
+            const user = await Users.findById(req.user.id).select("-password")
+            if(!user) return res.status(400).json({msg:"User doesn't exist"})
+
+            res.json(user);
+        } catch(err){
+            return res.status(500).json({msg:err.message});
+        }
     }
+
 }
 
 const createAccessToken = (user) =>{
