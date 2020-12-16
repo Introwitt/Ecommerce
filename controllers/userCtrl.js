9b74034cb39b59c4035f58a8e7a01bd0e7a83201
Harsh Kashyap <harshkashyap307@gmail.com>
2020-12-16 11:56:52 +0530

    Minor Adjustments

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,4 +1,5 @@
 const Users = require('../models/userModel'); 
+const Payments = require("../models/paymentModel");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -107,7 +108,35 @@ const userCtrl = {
         } catch(err){
             return res.status(500).json({msg:err.message});
         }
-    }
+    },
+
+    addCart: async (req, res) => {
+        try {
+          const user = await Users.findById(req.user.id);
+          if (!user) return res.status(400).json({ msg: "User does not exist." });
+    
+          await Users.findOneAndUpdate(
+            { _id: req.user.id },
+            {
+              cart: req.body.cart,
+            }
+          );
+    
+          return res.json({ msg: "Added to cart" });
+        } catch (err) {
+          return res.status(500).json({ msg: err.message });
+        }
+    },
+
+    history: async (req, res) => {
+        try {
+          const history = await Payments.find({ user_id: req.user.id });
+    
+          res.json(history);
+        } catch (err) {
+          return res.status(500).json({ msg: err.message });
+        }
+    },
 
 }
 
